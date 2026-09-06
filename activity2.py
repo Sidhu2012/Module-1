8010686708bc8c58150d10b8fb4d7e3e6de24b84
Siddhu@121212 <siddhantrawat2012@gmail.com>
2026-09-06 14:39:42 +0530

    uses of print  statement

---
+++ activity2.py
@@ -0,0 +1,14 @@
+# print a sentence
+print("i am siddhant")
+
+#print a number
+print(12)
+
+#print a number and a sentence
+print("iam siddhant",12)
+
+#print a new line
+print("i am siddhant",end="12")
+
+#print a next sentence
+print("iam siddhant\n welcome to my house")
