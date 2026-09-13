19ff28369e077f93b1073ff6537fbf6f169e36c4
Siddhu@121212 <siddhantrawat2012@gmail.com>
2026-09-13 15:13:22 +0530

    data types

---
+++ goals .py
@@ -0,0 +1,25 @@
+#  completing mygoal 
+ import keywords
+print (keywords.kwlist)
+
+#  performing a task to learn coding
+ print ("learn newskills to make my life esay")
+
+
+
+# asking three questions
+print ("what is my goal?")
+print("where it will help me?")
+print(" what is the use")
+
+# Use input() three times and store each answer in its own variable
+ sentence =input (" enter my first sentence")
+sentence=input ("enter my second sentence")
+sentence = input("enter my third sentence")
+
+# printing my name 
+x="siddhant"
+y= daily_minutes 30 
+print (x,y)
+
+name = input (" enter my name")
